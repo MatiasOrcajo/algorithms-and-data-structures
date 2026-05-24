242e028c93b71dd265968ede6ed5de4f745d27b0
MatiasOrcajo <matiasorcajo3@gmail.com>
2026-05-24 19:09:27 -0300

    Add delete method and refactor doubly linked list implementation in PHP

--- doubly_linked_list.php
+++ doubly_linked_list.php
@@ -18,7 +18,6 @@ class DoublyLinkedList
 {
 
     public Node|null $firstNode = null;
-    public Node|null $prevNode = null;
 
     public function insert(int $value)
     {
@@ -31,7 +30,6 @@ class DoublyLinkedList
         }
 
         while ($currentNode->nextNode !== null) {
-            $this->prevNode = $currentNode;
             $currentNode = $currentNode->nextNode;
         }
 
@@ -40,12 +38,27 @@ class DoublyLinkedList
 
     }
 
+    public function delete(int $value)
+    {
+        echo "Eliminando nodo con value $value \n\n";
+        $currentNode = $this->firstNode;
+        $prevNode = null;
+
+        while ($currentNode->value !== $value) {
+            $prevNode = $currentNode;
+            $currentNode = $currentNode->nextNode;
+        }
+
+        $currentNode->prevNode->nextNode = $currentNode->nextNode;
+        $currentNode->nextNode->prevNode = $prevNode;
+    }
+
     public function traverse()
     {
         $currentNode = $this->firstNode;
         while ($currentNode !== null) {
-            echo "Nodo actual: " . $currentNode->value . "\n";
             echo "Nodo previo: " . ($currentNode->prevNode->value ?? null) . "\n";
+            echo "Nodo actual: " . $currentNode->value . "\n";
             echo "Nodo siguiente: " . ($currentNode->nextNode->value ?? null) . "\n";
             echo "\n";
 
@@ -58,7 +71,7 @@ $list = new DoublyLinkedList();
 $list->insert(2);
 $list->insert(7);
 $list->insert(6);
-$list->insert(8);
-$list->insert(9);
 $list->insert(15);
+$list->traverse();
+$list->delete(6);
 $list->traverse();
